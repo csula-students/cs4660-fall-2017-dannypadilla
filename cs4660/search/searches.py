6ad0c26e93f4fd9a501ed5e5df9d37f6b29199ad
Danny Padilla <dpadilla010@gmail.com>
2017-10-13 18:43:27 -0700

    finish bfs

--- cs4660/search/searches.py
+++ cs4660/search/searches.py
@@ -1,6 +1,8 @@
 """
 Searches module defines all different search algorithms
 """
+import queue
+from graph import graph as g
 
 def bfs(graph, initial_node, dest_node):
     """
@@ -8,7 +10,34 @@ def bfs(graph, initial_node, dest_node):
     uses graph to do search from the initial_node to dest_node
     returns a list of actions going from the initial node to dest_node
     """
-    pass
+
+    frontier = queue.Queue()
+    explored_set = []
+    distances = {
+        initial_node: 0
+    }
+    parents = {
+        initial_node: None
+    }
+
+    frontier.put(initial_node)
+
+    while( not frontier.empty() ):
+        current_node = frontier.get() # take node from queue
+        
+        if(current_node == dest_node): # found path
+            return get_path(current_node, distances, parents)
+        
+        if(current_node not in explored_set):
+            explored_set.append(current_node)
+        for node in graph.neighbors(current_node):
+            if(node not in explored_set):
+                explored_set.append(node) # add child to explored set
+                frontier.put(node) # add child to queue
+                distances[node] = distances[current_node] + graph.distance(current_node, node) # calculate distance
+                parents[node] = current_node # assign parent to child
+                
+    return False # no path was found
 
 def dfs(graph, initial_node, dest_node):
     """
@@ -16,6 +45,7 @@ def dfs(graph, initial_node, dest_node):
     uses graph to do search from the initial_node to dest_node
     returns a list of actions going from the initial node to dest_node
     """
+    # stack
     pass
 
 def dijkstra_search(graph, initial_node, dest_node):
@@ -24,6 +54,7 @@ def dijkstra_search(graph, initial_node, dest_node):
     uses graph to do search from the initial_node to dest_node
     returns a list of actions going from the initial node to dest_node
     """
+    # priority queue
     pass
 
 def a_star_search(graph, initial_node, dest_node):
@@ -33,3 +64,16 @@ def a_star_search(graph, initial_node, dest_node):
     returns a list of actions going from the initial node to dest_node
     """
     pass
+
+def get_path(current_node, distances_d, parents_d):
+    path = []
+    parent_node = parents_d[current_node]
+    while(parent_node != None):
+        distance =  distances_d[current_node] - distances_d[parent_node]
+        edge = g.Edge(parent_node, current_node, distance) # should distance be from parent to node OR initial to dest node?
+        path.append(edge) # create new EDGE or get from graph?
+        current_node = parent_node # for next loop
+        parent_node = parents_d[current_node]
+    
+    path.reverse()
+    return path
